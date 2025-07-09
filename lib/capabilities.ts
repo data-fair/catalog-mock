9f9ca494137d5a63bf96b15729640d360e75f994
BatLeDev <baptiste.guerny@koumoul.com>
2025-07-09 10:46:29 +0200

    chore: small changes

--- lib/capabilities.ts
+++ lib/capabilities.ts
@@ -1,19 +1,20 @@
+import type { Capability } from '@data-fair/types-catalogs'
+
 /**
  * The list of capabilities of the plugin.
  * These capabilities define the actions that can be performed with the plugin.
- * We add "as const" to ensure the type is a list of literal type, not a list of strings.
- * This allows TypeScript to check if the plugin has the required funcitons for each capability.
+ * The capabilities must satisfy the `Capability` type.
  */
 export const capabilities = [
-  'import' as const,
-  'search' as const,
-  'pagination' as const,
-  'importConfig' as const,
-  'publishDataset' as const,
-  'deletePublication' as const,
-  'thumbnail' as const,
-  'thumbnailUrl' as const
-]
+  'import',
+  'search',
+  'pagination',
+  'importConfig',
+  'publishDataset',
+  'deletePublication',
+  'thumbnail',
+  'thumbnailUrl'
+] satisfies Capability[]
 
 export type MockCapabilities = typeof capabilities
 export default capabilities
